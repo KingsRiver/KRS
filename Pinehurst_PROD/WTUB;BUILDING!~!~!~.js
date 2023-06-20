d755af1f81dfe7aeab58983e16db6cf1532cf6d5
KingsRiver <jason@kingsriversolutions.com>
2023-06-20 10:29:04 -0700

    created by Gray Quarter Git Push Tool

---
+++ Pinehurst_PROD/WTUB;BUILDING!~!~!~.js
@@ -0,0 +1,17 @@
+if (wfTask == "Permit Issuance" && wfStatus == "Issued" && balanceDue > 0) {
+showMessage = true;
+cancel = true;
+comment("Cannot issue this permit with a balance greater than zero");
+}
+
+if (wfTask == "Permit Status" && wfStatus == "Permit Issued" && balanceDue > 0) {
+showMessage = true;
+cancel = true;
+comment("Cannot issue this permit with a balance greater than zero");
+}
+
+if (wfTask == "Review and Issuance" && wfStatus == "Closed" && balanceDue > 0) {
+showMessage = true;
+cancel = true;
+comment("Cannot issue this permit with a balance greater than zero");
+}
