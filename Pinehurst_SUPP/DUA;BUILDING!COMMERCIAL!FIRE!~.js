32b35e437ef3476e6af9c75dd1165e8da8054797
KingsRiver <jason@kingsriversolutions.com>
2023-06-20 10:21:42 -0700

    created by Gray Quarter Git Push Tool

---
+++ Pinehurst_SUPP/DUA;BUILDING!COMMERCIAL!FIRE!~.js
@@ -0,0 +1,13 @@
+showDebug=true;
+//logDebug("in dua with TestLoop" + testLoop);
+
+var params = aa.util.newHashtable();
+var altid = capId.getCustomID();
+addParameter(params, "$$ALTID$$", altid);
+
+//	pamaremeters.put(key, value);
+var rFiles = new Array();
+//	rFiles.push(rFile);
+logDebug("before: in if of DUA check testLoop");
+	aa.document.sendEmailByTemplateName("noreply@accela.com", "ktaylor@vopnc.org", "ktaylor@vopnc.org, ktaylor@vopnc.org", "NEW_DOCUMENT_UPLOADED", params, rFiles);
+logDebug("after: in dua");
